9122fa105fe01abab0661eccef72e1c57520684d
kaBeech <contact@kaBeech.com>
2023-06-12 21:29:28 -0700

    Add types.ts

---
+++ util/types.ts
@@ -0,0 +1,7 @@
+type Ascension = [number, number, number];
+
+type Declination = [number, number, number];
+
+type Coordinates = [Ascension, Declination];
+
+export type { Ascension, Coordinates, Declination };
