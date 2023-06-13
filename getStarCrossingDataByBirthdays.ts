d79e9d6533f18b05c758a2867a156543be4d62d9
kaBeech <contact@kaBeech.com>
2023-06-13 00:07:28 -0700

    Fix HTTP typo

--- getStarCrossingDataByBirthdays.ts
+++ getStarCrossingDataByBirthdays.ts
@@ -9,8 +9,8 @@ const stars: Star[] = await getAllStars()
 
 const getStarCrossing = (birthdays: string) => {
     const birthdaysArray = birthdays.split(",") as [string, string];
-    const birthday1 = birthdaysArray[0].split("/") as [string, string];
-    const birthday2 = birthdaysArray[1].split("/") as [string, string];
+    const birthday1 = birthdaysArray[0].split(";") as [string, string];
+    const birthday2 = birthdaysArray[1].split(";") as [string, string];
     const birthday1OfYear = convertMonthAndDayToDayOfYear(+birthday1[0], +birthday1[1])
     const birthday2OfYear = convertMonthAndDayToDayOfYear(+birthday2[0], +birthday2[1])
     const starCrossing: Coordinates = [convertDayToAscension(birthday1OfYear), convertDayToDeclination(birthday2OfYear)]
