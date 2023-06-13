4ebdccf0b0e176e18efc2a0b6f04f58e8bd55427
kaBeech <contact@kaBeech.com>
2023-06-12 21:07:34 -0700

    Add conversion functions

---
+++ util/convertDayToAscension.ts
@@ -0,0 +1,14 @@
+const dayAscensionCoefficient = 24 * 60 * 60;
+
+const convertDayToAscension = (dayOfYear: number) => {
+  const rawAscension = (dayOfYear / 366) * dayAscensionCoefficient;
+  let treatedAscension = rawAscension / 24;
+  //   check these for accuracy:
+  const hours = Math.floor(treatedAscension);
+  treatedAscension = (treatedAscension - hours) * 100 / 60;
+  const minutes = Math.floor(treatedAscension);
+  treatedAscension = (treatedAscension - minutes) * 100 / 60;
+  const seconds = treatedAscension.toPrecision(4);
+  const ascension = [hours, minutes, seconds];
+  return ascension;
+};
---
+++ util/convertDayToDeclination.ts
@@ -0,0 +1,14 @@
+const dayDeclinationCoefficient = 180 * 60 * 60;
+
+const convertDayToDeclination = (dayOfYear: number) => {
+  const rawDeclination = (dayOfYear / 366) * dayDeclinationCoefficient;
+  let treatedDeclination = rawDeclination / 180;
+  //   check these for accuracy:
+  const hours = Math.floor(treatedDeclination);
+  treatedDeclination = (treatedDeclination - hours) * 100 / 60;
+  const minutes = Math.floor(treatedDeclination);
+  treatedDeclination = (treatedDeclination - minutes) * 100 / 60;
+  const seconds = treatedDeclination.toPrecision(4);
+  const declination = [hours, minutes, seconds];
+  return declination;
+};
