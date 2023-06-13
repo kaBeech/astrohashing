4187dc4aeffc45483eff1c583e41b4d2ce8cd6b2
kaBeech <contact@kaBeech.com>
2023-06-13 12:40:09 -0700

    Update populateDB script

--- scripts/populateDB.ts
+++ scripts/populateDB.ts
@@ -1,1 +1,8 @@
+import { upsertStar } from "../db.ts";
 import getStarCatalog from "../util/getStarCatalog.ts";
+
+const starCatalog = getStarCatalog();
+
+for (const star of starCatalog) {
+  upsertStar(star);
+}
