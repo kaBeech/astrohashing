1ec616637749b54629044a9aeb13e88b0b85a707
kaBeech <contact@kaBeech.com>
2023-06-14 23:01:28 -0700

    Revise test

--- tests/getGreatCircleDistanceBetweenCoordinates_test.ts
+++ tests/getGreatCircleDistanceBetweenCoordinates_test.ts
@@ -5,25 +5,31 @@ import {
 import getGreatCircleDistanceBetweenCoordinates from "../util/getGreatCircleDistanceBetweenCoordinates.ts";
 
 Deno.test("inputting same coordinates returns 0 radians", () => {
-  const result = getGreatCircleDistanceBetweenCoordinates([0, 0], [0, 0]);
+  const result = getGreatCircleDistanceBetweenCoordinates([[0, 0, 0], [
+    0,
+    0,
+    0,
+  ]], [[0, 0, 0], [0, 0, 0]]);
 
   assertEquals(result, 0);
 });
 
 Deno.test("inputting extreme ascension coordinates returns pi radians", () => {
-  const result = getGreatCircleDistanceBetweenCoordinates([0, 0], [
-    3.14159,
+  const result = getGreatCircleDistanceBetweenCoordinates([[0, 0, 0], [
     0,
-  ]);
+    0,
+    0,
+  ]], [[12, 0, 0], [0, 0, 0]]);
 
   assertAlmostEquals(result, 3.14159);
 });
 
 Deno.test("inputting extreme declination coordinates returns pi radians", () => {
-  const result = getGreatCircleDistanceBetweenCoordinates([0, 0], [
+  const result = getGreatCircleDistanceBetweenCoordinates([[-90, 0, 0], [
     0,
-    3.14159,
-  ]);
+    0,
+    0,
+  ]], [[0, 0, 0], [90, 0, 0]]);
 
   assertAlmostEquals(result, 3.14159);
 });
