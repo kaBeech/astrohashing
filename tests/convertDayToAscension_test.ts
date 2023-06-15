f71edb824b59d6f0363ccdca62da88c119d2d4c8
kaBeech <contact@kaBeech.com>
2023-06-14 23:04:41 -0700

    Revise test

--- tests/convertDayToAscension_test.ts
+++ tests/convertDayToAscension_test.ts
@@ -7,10 +7,10 @@ Deno.test("inputting day 1 returns -90d 0m 0s", () => {
   assertEquals(result, [0, 0, 0]);
 });
 
-Deno.test("inputting day 366 returns 90d 0m 0s", () => {
-  const result = convertDayToAscension(71);
+Deno.test("inputting day 366 returns 24h 0m 0s", () => {
+  const result = convertDayToAscension(366);
 
-  assertEquals(result, [90, 0, 0]);
+  assertEquals(result, [24, 0, 0]);
 });
 
 Deno.test("inputting day 72 returns 4d 40m 60s", () => {
