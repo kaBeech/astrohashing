75efdd02d25cee933f2df6b37732fe8b808f1d53
kaBeech <contact@kaBeech.com>
2023-06-14 21:29:29 -0700

    Add convertCoordinatesToRadians equations

--- util/convertCoordinatesToRadians.ts
+++ util/convertCoordinatesToRadians.ts
@@ -1,10 +1,20 @@
 import { Ascension, Declination } from "../types.ts";
 
+const ARC_SECONDS_IN_A_CIRCLE = 360 * 60 * 60;
+
 const convertAscensionToRadians = (ascension: Ascension) => {
-  return ascension[0];
+  const ascensionArcSeconds = (ascension[0] / 24) * 360 * 3600 +
+    ascension[1] * 60 + ascension[2];
+  const radians = (ascensionArcSeconds * 2 * Math.PI) /
+    ARC_SECONDS_IN_A_CIRCLE;
+  return radians;
 };
 const convertDeclinationToRadians = (declination: Declination) => {
-  return declination[0];
+  const declinationArcSeconds = (declination[0] + 90) * 3600 +
+    declination[1] * 60 + declination[2];
+  const radians = (declinationArcSeconds * 2 * Math.PI) /
+    ARC_SECONDS_IN_A_CIRCLE;
+  return radians;
 };
 
 export { convertAscensionToRadians, convertDeclinationToRadians };
