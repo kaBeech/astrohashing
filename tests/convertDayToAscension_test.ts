7c711f5b4c06a36af7a38f8ad561d36d8571f42d
kaBeech <contact@kaBeech.com>
2023-06-14 20:09:17 -0700

    Revise convertDayToAscension test

--- tests/convertDayToAscension_test.ts
+++ tests/convertDayToAscension_test.ts
@@ -1,22 +1,22 @@
 import { assertEquals } from "https://deno.land/std@0.188.0/testing/asserts.ts";
-import { Coordinates } from "../types.ts";
 import convertDayToAscension from "../util/convertDayToAscension.ts";
 
-const starCrossing: Coordinates = [
-  [0, 0, 1],
-  [-4, 3, 13],
-];
+Deno.test("inputting day 1 returns -90d 0m 0s", () => {
+  const result = convertDayToAscension(1);
 
-Deno.test("testing convert day to ascension on day 0", () => {
-  const result1 = convertDayToAscension(0);
-
-  assertEquals(result1, [0, 0, 0]);
+  assertEquals(result, [0, 0, 0]);
 });
 
-Deno.test("testing convert day to ascension on day 0", () => {
-  const result1 = convertDayToAscension(71);
+Deno.test("inputting day 366 returns 90d 0m 0s", () => {
+  const result = convertDayToAscension(71);
 
-  assertEquals(result1, [4, 40, 6]);
+  assertEquals(result, [90, 0, 0]);
+});
+
+Deno.test("inputting day 72 returns 4d 40m 60s", () => {
+  const result = convertDayToAscension(72);
+
+  assertEquals(result, [4, 40, 6]);
   // (71 / 365) * 24 = 4.6... => 4
   // remainder of .6684 * 60 = 40.109... => 40
   // .109... * 60 = 6.57.. => 6 or 7 seconds => Are we rounding or truncating here?
