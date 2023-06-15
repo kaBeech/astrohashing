6cf0c7c6690facc95a3f0ded96d8c94bc7353bbc
kaBeech <contact@kaBeech.com>
2023-06-14 22:41:22 -0700

    Add convertRadiansToCoordinates equations

--- util/convertRadiansToCoordinates.ts
+++ util/convertRadiansToCoordinates.ts
@@ -1,8 +1,49 @@
-const convertRadiansToAscension = (radians: number) => {
-  return [radians, radians];
+import { Ascension, Declination } from "../types.ts";
+
+// const ARC_SECONDS_IN_A_CIRCLE = 360 * 60 * 60;
+
+//   const declinationArcSeconds = (ARC_SECONDS_IN_A_CIRCLE * radians) /
+//     (2 * Math.PI);
+
+const convertRadiansToAscension = (radians: number): Ascension => {
+  const ascensionInArcSeconds = radians / (2 * Math.PI) * 24 * 60 * 60;
+  const ascensionHours = Math.floor(ascensionInArcSeconds / (60 * 60));
+  const ascensionMinutes = Math.floor(
+    (ascensionInArcSeconds - (ascensionHours * 60 * 60)) / 60,
+  );
+  const ascensionSeconds = Math.floor(
+    ascensionInArcSeconds - (ascensionHours * 60 * 60) -
+      (ascensionMinutes * 60),
+  );
+  const ascension: Ascension = [
+    ascensionHours,
+    ascensionMinutes,
+    ascensionSeconds,
+  ];
+  return ascension;
 };
-const convertRadiansToDeclination = (radians: number) => {
-  return [radians, radians];
+const convertRadiansToDeclination = (radians: number): Declination => {
+  const declinationInArcSeconds = radians / Math.PI * 180 * 60 * 60;
+  const declinationDegrees = Math.floor(declinationInArcSeconds / (60 * 60)) -
+    90;
+  const declinationMinutes = Math.floor(
+    (declinationInArcSeconds - ((declinationDegrees + 90) * 60 * 60)) / 60,
+  );
+  const declinationSeconds = Math.floor(
+    declinationInArcSeconds - ((declinationDegrees + 90) * 60 * 60) -
+      (declinationMinutes * 60),
+  );
+  const declination: Declination = [
+    declinationDegrees,
+    declinationMinutes,
+    declinationSeconds,
+  ];
+  return declination;
 };
+
+// const declinationArcSeconds2 = (declination[0] + 90) * 3600 +
+// declination[1] * 60 + declination[2];
+
+// (declination[0] + 90) * 3600 + declination[1] * 60 + declination[2] = declinationArcSeconds
 
 export { convertRadiansToAscension, convertRadiansToDeclination };
