7870cabde1f1e65208575e96629c381996df6d01
kaBeech <contact@kaBeech.com>
2023-06-14 20:00:38 -0700

    Cleanup

--- util/convertDayToDeclinationAngle.ts
+++ util/convertDayToDeclinationAngle.ts
@@ -1,10 +1,6 @@
-import { Declination } from "../types.ts";
-
-const dayDeclinationCoefficient = 180 * 60 * 60;
-
 const convertDayToDeclinationAngle = (dayOfYear: number): number => {
-  const declination: number = ((dayOfYear - 1) / 365) * Math.PI;
-  return declination;
+  const declinationAngle: number = ((dayOfYear - 1) / 365) * Math.PI;
+  return declinationAngle;
 };
 
 export default convertDayToDeclinationAngle;
