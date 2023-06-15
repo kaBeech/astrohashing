339206c28e6947588d6703b5f0a080d2a1610609
kaBeech <contact@kaBeech.com>
2023-06-14 19:49:55 -0700

    Add convertDayToDeclinationAngle test

---
+++ tests/convertDayToDeclinationAngle_test.ts
@@ -0,0 +1,14 @@
+import { assertEquals } from "https://deno.land/std@0.188.0/testing/asserts.ts";
+import convertDayToDeclinationAngle from "../util/convertDayToDeclinationAngle.ts";
+
+Deno.test("inputting day 1 returns 0 radians", () => {
+  const result = convertDayToDeclinationAngle(0);
+
+  assertEquals(result, 0);
+});
+
+Deno.test("inputting day 366 returns pi radians", () => {
+  const result1 = convertDayToDeclinationAngle(366);
+
+  assertEquals(result1, 3.1415926535);
+});
