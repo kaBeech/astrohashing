3cb2b3c072658dff7d34c9a688356d7aeb66218b
kaBeech <contact@kaBeech.com>
2023-06-14 20:01:27 -0700

    Revise equation so that Day 1 returns 0 degrees

--- util/convertDayToAscensionAngle.ts
+++ util/convertDayToAscensionAngle.ts
@@ -3,7 +3,7 @@ import { Ascension } from "../types.ts";
 const dayAscensionCoefficient = 24 * 60 * 60;
 
 const convertDayToAscensionAngle = (dayOfYear: number): number => {
-  const ascension: number = (dayOfYear / 366) * 2 * Math.PI;
+  const ascension: number = ((dayOfYear - 1) / 365) * 2 * Math.PI;
   return ascension;
 };
 
