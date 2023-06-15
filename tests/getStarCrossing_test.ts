e461c1e34b790406cd673c765eb01955ef381e03
kaBeech <contact@kaBeech.com>
2023-06-14 23:14:15 -0700

    Revise test

--- tests/getStarCrossing_test.ts
+++ tests/getStarCrossing_test.ts
@@ -3,11 +3,11 @@ import getStarCrossing from "../getStarCrossing.ts";
 
 const birthdays = "1960-1-1,1960-7-2";
 
-Deno.test("inputting properly formatted birthdays returns proper coordinates", () => {
+Deno.test("inputting '1960-1-1,1960-7-2' returns proper coordinates", () => {
   const result1 = getStarCrossing(birthdays);
 
   assertEquals(
     result1,
-    [[0, 0, 0], [0, 0, 0]],
+    [[0, 0, 0], [0, 14, 47]],
   );
 });
