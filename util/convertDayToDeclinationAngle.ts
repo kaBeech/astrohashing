32f64493824c56e1be83a6901063d28a26d28fc4
kaBeech <contact@kaBeech.com>
2023-06-14 19:59:06 -0700

    Revise equation so that Day 1 returns 0 degrees

--- util/convertDayToDeclinationAngle.ts
+++ util/convertDayToDeclinationAngle.ts
@@ -3,7 +3,7 @@ import { Declination } from "../types.ts";
 const dayDeclinationCoefficient = 180 * 60 * 60;
 
 const convertDayToDeclinationAngle = (dayOfYear: number): number => {
-  const declination: number = (dayOfYear / 366) * Math.PI;
+  const declination: number = ((dayOfYear - 1) / 365) * Math.PI;
   return declination;
 };
 
