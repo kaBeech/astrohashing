ea6c91533c2e8ce4fc60c311503fb43f6a2d78f9
kaBeech <contact@kaBeech.com>
2023-06-15 03:10:21 -0700

    Remove on-request commonName and infoURL caching

--- getClosestStar.ts
+++ getClosestStar.ts
@@ -1,12 +1,9 @@
-import { getAllStars, updateStarCommonName, updateStarInfoURL } from "./db.ts";
-import getInfoURL from "./getInfoURL.ts";
+import { getAllStars } from "./db.ts";
 import { Coordinates, Star } from "./types.ts";
-// import { fetchAndParseHTML } from "./util/fetchAndParse.ts";
 import getGreatCircleDistanceBetweenCoordinates from "./util/getGreatCircleDistanceBetweenCoordinates.ts";
 
 const stars: Star[] = await getAllStars() as unknown as Star[];
 
-// const getClosestStar = async (starCrossing: Coordinates) => {
 const getClosestStar = (starCrossing: Coordinates) => {
   let closestDistance: number | null = null;
   let closestStar: Star | null = null;
@@ -22,37 +19,6 @@ const getClosestStar = (starCrossing: Coordinates) => {
   }
   if (closestStar === null) {
     throw new Error("No stars found");
-  }
-  if (closestStar.commonName === null) {
-    const infoURL = getInfoURL(closestStar.name);
-    // let commonName = await fetchAndParseHTML(infoURL);
-    let commonName = "Hmm<H1>Test Fetch</H1>Weird";
-    if (commonName.indexOf("H1") > -1) {
-      commonName = commonName.split("H1")[1];
-      commonName = commonName.slice(1, -2);
-      updateStarCommonName(closestStar, commonName);
-      closestStar.commonName = commonName;
-      updateStarInfoURL(closestStar, infoURL);
-      closestStar.infoURL = infoURL;
-    } else if (closestStar.altName !== null) {
-      const altName = closestStar.altName.replace("HIP", "HIC");
-      const infoAltURL = getInfoURL(altName);
-      // commonName = await fetchAndParseHTML(infoAltURL);
-      commonName = "Hmm<H1>Test FetchAgain</H1>Weird";
-      if (commonName.indexOf("H1") > -1) {
-        commonName = commonName.split("H1")[1];
-        commonName = commonName.slice(1, -2);
-        updateStarCommonName(closestStar, commonName);
-        closestStar.commonName = commonName;
-        updateStarInfoURL(closestStar, infoAltURL);
-        closestStar.infoURL = infoAltURL;
-      }
-    } else {
-      console.log("commonName not found in ISDB");
-      const fallbackURL =
-        `https://duckduckgo.com/?q=!ducky+${closestStar.name}`;
-      updateStarInfoURL(closestStar, fallbackURL);
-    }
   }
   return closestStar;
 };
