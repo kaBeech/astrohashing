9c26f2f05451dc4dc45340e2886f870f63d51a1e
kaBeech <contact@kaBeech.com>
2023-06-15 03:08:45 -0700

    Add updateCommonNamesAndInfoURLs script

---
+++ scripts/updateCommonNamesAndInfoURLs.ts
@@ -0,0 +1,43 @@
+import { updateStarCommonName, updateStarInfoURL } from "../db.ts";
+import getInfoURL from "../getInfoURL.ts";
+import { fetchAndParseHTML } from "../util/fetchAndParse.ts";
+import getStarCatalog from "../util/getStarCatalog.ts";
+
+const starCatalog = getStarCatalog();
+let i = 0;
+for (const star of starCatalog) {
+  if (i < 10) {
+    if (star.commonName === null) {
+      const infoURL = getInfoURL(star.name);
+      let commonName = await fetchAndParseHTML(infoURL);
+      // let commonName = "Hmm<H1>Test Fetch</H1>Weird";
+      if (commonName.indexOf("H1") > -1) {
+        commonName = commonName.split("H1")[1];
+        commonName = commonName.slice(1, -2);
+        updateStarCommonName(star, commonName);
+        star.commonName = commonName;
+        updateStarInfoURL(star, infoURL);
+        star.infoURL = infoURL;
+      } else if (star.altName !== null) {
+        const altName = star.altName.replace("HIP", "HIC");
+        const infoAltURL = getInfoURL(altName);
+        commonName = await fetchAndParseHTML(infoAltURL);
+        //   commonName = "Hmm<H1>Test FetchAgain</H1>Weird";
+        if (commonName.indexOf("H1") > -1) {
+          commonName = commonName.split("H1")[1];
+          commonName = commonName.slice(1, -2);
+          updateStarCommonName(star, commonName);
+          star.commonName = commonName;
+          updateStarInfoURL(star, infoAltURL);
+          star.infoURL = infoAltURL;
+        }
+      } else {
+        //   console.log("commonName not found in ISDB");
+        const fallbackURL = `https://duckduckgo.com/?q=!ducky+${star.name}`;
+        updateStarInfoURL(star, fallbackURL);
+      }
+      setTimeout(() => {}, 500);
+    }
+    i++;
+  }
+}
