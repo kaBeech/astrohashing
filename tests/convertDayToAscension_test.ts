5e1a4f59e83ced2bdf8aa2d2f7b53253a6098f6a
kaBeech <contact@kaBeech.com>
2023-06-15 03:30:39 -0700

    Cleanup

--- tests/convertDayToAscension_test.ts
+++ tests/convertDayToAscension_test.ts
@@ -17,7 +17,4 @@ Deno.test("inputting day 72 returns 4d 40m 60s", () => {
   const result = convertDayToAscension(72);
 
   assertEquals(result, [4, 40, 6]);
-  // (71 / 365) * 24 = 4.6... => 4
-  // remainder of .6684 * 60 = 40.109... => 40
-  // .109... * 60 = 6.57.. => 6 or 7 seconds => Are we rounding or truncating here?
 });
