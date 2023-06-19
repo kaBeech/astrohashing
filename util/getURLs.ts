dd452ad8caf657072e0b6d8ca07dcdc73f9cf73c
kaBeech <contact@kaBeech.com>
2023-06-19 01:15:20 -0700

    Collate URL getters

---
+++ util/getURLs.ts
@@ -0,0 +1,56 @@
+import { Coordinates } from "../types.ts";
+import {
+  convertAscensionToRadians,
+  convertDeclinationToRadians,
+} from "../util/convertCoordinatesToRadians.ts";
+import {
+  convertRadiansToURLFriendlyAscension,
+  convertRadiansToURLFriendlyDeclination,
+} from "../util/convertRadiansToURLFriendly.ts";
+
+const getISDBURL = (starName: string): string => {
+  const starNameSplit = starName.split(" ");
+  const infoURL =
+    `http://www.stellar-database.com/Scripts/search_star.exe?Catalog=${
+      starNameSplit[0]
+    }&CatNo=${starNameSplit[1]}`;
+  return infoURL;
+};
+
+const getSkyMapURL = (starCrossing: Coordinates): string => {
+  const urlFriendlyAscension = convertRadiansToURLFriendlyAscension(
+    convertAscensionToRadians(starCrossing[0]),
+  );
+  const urlFriendlyDeclination = convertRadiansToURLFriendlyDeclination(
+    convertDeclinationToRadians(starCrossing[1]),
+  );
+  // const skyMapURL = `http://www.wikisky.org/?ra=6.3991971944444&de=-52.695660555556&zoom=4&show_grid=1&show_constellation_lines=1&show_constellation_boundaries=1&show_const_names=1&show_galaxies=1&img_source=DSS2`
+  const skyMapURL =
+    // `http://www.wikisky.org/?ra=${urlFriendlyAscension}&de=${urlFriendlyDeclination}&zoom=3&show_grid=1&show_constellation_lines=1&show_constellation_boundaries=1&show_const_names=1&show_galaxies=1&img_source=DSS2`;
+    `http://server1.sky-map.org/skywindow?ra=${urlFriendlyAscension}&de=${urlFriendlyDeclination}&zoom=2.5&show_grid=1&show_constellation_lines=1&show_constellation_boundaries=1&show_const_names=1&show_galaxies=1&img_source=DSS2`;
+  return skyMapURL;
+};
+
+const getStaticPhotoURL = (starCrossing: Coordinates): string => {
+  const urlFriendlyAscension = convertRadiansToURLFriendlyAscension(
+    convertAscensionToRadians(starCrossing[0]),
+  );
+  const urlFriendlyDeclination = convertRadiansToURLFriendlyDeclination(
+    convertDeclinationToRadians(starCrossing[1]),
+  );
+
+  const skyMapURL =
+    `http://www.wikisky.org/imgcut?survey=DSS2&img_id=all&angle=15&ra=${urlFriendlyAscension}&de=${urlFriendlyDeclination}&width=800&height=800&projection=tan&interpolation=bicubic&jpeg_quality=0.8&output_type=jpeg`;
+  return skyMapURL;
+};
+
+const getUniverseGuideURL = (starName: string): string => {
+  const starNameSplit = starName.split(" ");
+  const universeGuideURL =
+    `https://duckduckgo.com/?q=!ducky+site%3Awww.universeguide.com+star+${
+      starNameSplit[0]
+    }+${starNameSplit[1]}`;
+  return universeGuideURL;
+};
+
+export { getISDBURL, getSkyMapURL, getStaticPhotoURL, getUniverseGuideURL };
