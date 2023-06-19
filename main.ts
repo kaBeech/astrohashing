1fb0e306f4609993ae265b05a42de097bb8888d1
kaBeech <contact@kaBeech.com>
2023-06-19 13:35:23 -0700

    Comment out unused import

--- main.ts
+++ main.ts
@@ -7,7 +7,7 @@ import {
 import getStarCrossingDataByBirthdays from "./getStarCrossingDataByBirthdays.ts";
 import { getAllStars, upsertStar } from "./db.ts";
 import createStarCatalog from "./util/createStarCatalog.ts";
-import updateAllCommonNamesAndInfoURLs from "./updateAllCommonNamesAndInfoURLs.ts";
+// import updateAllCommonNamesAndInfoURLs from "./updateAllCommonNamesAndInfoURLs.ts";
 import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 
 // Populate DB
